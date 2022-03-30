3754e8f85fd9b0314e0f15935220f4b1be70d256
Jun Zhang <jun@junz.org>
2022-03-30 19:10:27 +0800

    Try conan.
    Signed-off-by: Jun Zhang <jun@junz.org>

--- test/test_lexer.cc
+++ test/test_lexer.cc
@@ -1,16 +1,21 @@
-#include <iostream>
 #include <string_view>
 
+#include "gtest/gtest.h"
 #include "jcc/lexer.h"
 #include "jcc/token.h"
 
-int main() {
+TEST(LexerTest, Basic) {
   std::string_view code = R"(int main() {return 1;})";
   const char* start = code.data();
   const char* end = code.data() + code.size();
   Lexer lexer{start, end};
-  while (!lexer.done()) {
-    Token tok = lexer.lex();
-    std::cout << tok.getName() << "\n";
-  }
+  EXPECT_EQ(TokenKind::Int, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::Identifier, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::LeftParen, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::RightParen, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::LeftBracket, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::Return, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::NumericConstant, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::Semi, lexer.lex().getKind());
+  EXPECT_EQ(TokenKind::RightBracket, lexer.lex().getKind());
 }
