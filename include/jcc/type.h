7c5f20e5dd60da5d4617904e5bab84d1371e0fe1
Jun Zhang <jun@junz.org>
2022-05-14 15:50:33 +0800

    Use bitmask in Type.
    Signed-off-by: Jun Zhang <jun@junz.org>

--- include/jcc/type.h
+++ include/jcc/type.h
@@ -1,122 +1,98 @@
 #pragma once
 
-enum class Qualifiers { Const, Restrict, Volatile, None };
+#include <concepts>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
 
-enum class Kind {
-  Void,
-  Bool,
-  Char,
-  Short,
-  Int,
-  Long,
-  Float,
-  Double,
-  Ldouble,
-  Enum,
-  Ptr,
-  Func,
-  Array,
-  Vla,  // variable-length array
-  Struct,
-  Union,
+enum class Qualifiers : std::uint8_t {
+  Const = 1 << 0,
+  Restrict = 1 << 1,
+  Volatile = 1 << 2,
+};
+
+// FIXME: Do we really need bitmask here?
+enum class Kind : std::uint32_t {
+  Void = 1 << 0,
+  Bool = 1 << 1,
+  Char = 1 << 2,
+  Short = 1 << 3,
+  Int = 1 << 4,
+  Long = 1 << 5,
+  Float = 1 << 6,
+  Double = 1 << 7,
+  Ldouble = 1 << 8,
+  Enum = 1 << 9,
+  Ptr = 1 << 10,
+  Func = 1 << 11,
+  Array = 1 << 12,
+  Vla = 1 << 13,  // variable-length array
+  Struct = 1 << 14,
+  Union = 1 << 15,
 };
 
 class Type {
-  Qualifiers quals_;
-  Kind kind_;
+  std::string name_;
+  std::uint32_t kind_;
 
   int size_;
   int alignment_;
 
+  std::uint8_t quals_;
+
  public:
   Type() = default;
 
-  Type(Qualifiers quals, Kind kind) : quals_(quals), kind_(kind) {
-    switch (kind_) {
-      case Kind::Void:
-        setSizeAlign(1, 1);
-        break;
-      case Kind::Bool:
-        setSizeAlign(1, 1);
-        break;
-      case Kind::Char:
-        setSizeAlign(1, 1);
-        break;
+  explicit Type(Kind kind) : kind_(static_cast<std::uint32_t>(kind)) {}
 
-      case Kind::Short:
-        setSizeAlign(2, 2);
-        break;
+  Type(Kind kind, std::string name)
+      : kind_(static_cast<std::uint32_t>(kind)), name_(std::move(name)) {}
 
-      case Kind::Int:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Long:
-        setSizeAlign(8, 8);
-        break;
-
-      case Kind::Float:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Double:
-        setSizeAlign(8, 8);
-        break;
-
-      case Kind::Ldouble:
-        setSizeAlign(16, 16);
-        break;
-
-      case Kind::Enum:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Ptr:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Func:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Array:
-        setSizeAlign(4, 4);
-        break;
-
-      case Kind::Vla:
-        setSizeAlign(8, 8);
-        break;
-
-      case Kind::Struct:
-        setSizeAlign(0, 1);
-        break;
-
-      case Kind::Union:
-        setSizeAlign(0, 1);
-        break;
-    }
+  template <typename Ty>
+  requires std::convertible_to<Ty, Type> Ty* asType() {
+    return static_cast<Ty*>(this);
   }
-
-  [[nodiscard]] Kind getKind() const { return kind_; }
 
   [[nodiscard]] int getSize() const { return size_; }
 
   [[nodiscard]] int getAlignment() const { return alignment_; }
 
-  [[nodiscard]] bool isConst() const { return quals_ == Qualifiers::Const; }
+  template <Qualifiers qualifier, Qualifiers... otherQuals>
+  void setQualifiers() {
+    quals_ |= static_cast<std::uint8_t>(qualifier);
+    if constexpr (sizeof...(otherQuals) > 0) {
+      setQualifiers<otherQuals...>();
+    }
+  }
+
+  [[nodiscard]] bool isConst() const {
+    return (quals_ & static_cast<std::uint8_t>(Qualifiers::Const)) != 0;
+  }
 
   [[nodiscard]] bool isRestrict() const {
-    return quals_ == Qualifiers::Restrict;
+    return (quals_ & static_cast<std::uint8_t>(Qualifiers::Restrict)) != 0;
   }
 
   [[nodiscard]] bool isVolatile() const {
-    return quals_ == Qualifiers::Volatile;
+    return (quals_ & static_cast<std::uint8_t>(Qualifiers::Volatile)) != 0;
+  }
+
+  [[nodiscard]] bool hasQualifiers() const { return quals_ != 0; }
+
+  template <Kind kind, Kind... otherKinds>
+  void setKinds() {
+    kind_ |= static_cast<std::uint8_t>(kind);
+    if constexpr (sizeof...(otherKinds) > 0) {
+      setKinds<otherKinds...>();
+    }
   }
 
   template <Kind ty, Kind... tyKinds>
   [[nodiscard]] bool is() const {
     if constexpr (sizeof...(tyKinds) != 0) {
-      return kind_ == ty || is<tyKinds...>();
+      return (kind_ & static_cast<std::uint32_t>(ty)) || is<tyKinds...>();
     }
     return false;
   }
@@ -131,7 +107,12 @@ class Type {
 
   [[nodiscard]] bool isNumeric() const { return isInteger() || isFloating(); }
 
- private:
+  [[nodiscard]] bool isPointer() const { return this->is<Kind::Ptr>(); }
+
+  [[nodiscard]] std::string_view getName() const { return name_; }
+
+  void setName(std::string name) { name_ = std::move(name); }
+
   void setSizeAlign(int size, int alignment) {
     size_ = size;
     alignment_ = alignment;
