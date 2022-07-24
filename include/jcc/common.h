1a3d9b50fae28e2891d5db68d0cb78dc56a915a0
Jun Zhang <jun@junz.org>
2022-07-24 16:26:08 +0800

    abort after hit unreable code
    Signed-off-by: Jun Zhang <jun@junz.org>

--- include/jcc/common.h
+++ include/jcc/common.h
@@ -2,11 +2,13 @@
 #include <fmt/format.h>
 
 #include <cassert>
+#include <cstdlib>
 
 #if !defined(_MSC_VER)
 #define jcc_unreachable()                                            \
   do {                                                               \
     fmt::print("Unexpected error in : {}:{}\n", __FILE__, __LINE__); \
+    std::abort();                                                    \
   } while (0)
 #else
 #define jcc_unreachable() assert(0);
