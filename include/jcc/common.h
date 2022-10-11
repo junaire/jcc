d01103d9c907a51723e08cc5bf436cf64dc231d7
Jun Zhang <jun@junz.org>
2022-10-11 14:03:43 +0800

    Dump the function we're executing when panic
    Signed-off-by: Jun Zhang <jun@junz.org>

--- include/jcc/common.h
+++ include/jcc/common.h
@@ -1,15 +1,11 @@
 #pragma once
 #include <fmt/format.h>
 
-#include <cassert>
 #include <cstdlib>
 
-#if !defined(_MSC_VER)
-#define jcc_unreachable()                                            \
-  do {                                                               \
-    fmt::print("Unexpected error in : {}:{}\n", __FILE__, __LINE__); \
-    std::abort();                                                    \
+#define jcc_unreachable()                                      \
+  do {                                                         \
+    fmt::print("\nUnreachable code executed in: {} ({}:{})\n", \
+               __PRETTY_FUNCTION__, __FILE__, __LINE__);       \
+    std::abort();                                              \
   } while (0)
-#else
-#define jcc_unreachable() assert(0);
-#endif
