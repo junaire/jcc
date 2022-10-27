52705eb3a9b0a89ff715f0206eff0d938661dd2b
Jun Zhang <jun@junz.org>
2022-10-27 11:08:45 +0800

    Make it easier to debug the tests
    Signed-off-by: Jun Zhang <jun@junz.org>

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# This will cause the shell to exit immediately if a simple command exits with a nonzero exit value.
+set -e
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+JCC=${DIR}/../build/bin/jcc
+
 for i in $(ls ${DIR}/*.c); do
-    ${DIR}/../build/bin/jcc ${i} --ast-dump
+    echo "Running" ${i}
+    ${JCC} ${i} --ast-dump
 done
