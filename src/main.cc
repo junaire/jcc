8a35d2c8052703e14fa5e5d2b245a807237b5530
Jun Zhang <jun@junz.org>
2022-11-07 20:46:16 +0800

    Report a nice message when use it in a incorrect way
    Signed-off-by: Jun Zhang <jun@junz.org>

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 
 #include <cstring>
 #include <fstream>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -11,29 +12,36 @@
 #include "jcc/lexer.h"
 #include "jcc/parser.h"
 
-static std::string ReadFile(std::string_view name) {
+static std::optional<std::string> ReadFile(std::string_view name) {
   std::ifstream file{name.data()};
   std::string contents{std::istreambuf_iterator<char>(file),
                        std::istreambuf_iterator<char>()};
+  if (contents.empty()) {
+    return std::nullopt;
+  }
   return contents;
 }
 
 int main(int argc, char** argv) {
   if (argc < 2) {
-    fmt::print("Please at least pass one argument to JCC!");
+    fmt::print("Please at least pass one argument to JCC!\n");
     return 1;
   }
 
   bool ast_dump_mode = false;
   std::string file_name = argv[1];
-  std::string content = ReadFile(file_name);
+  std::optional<std::string> content = ReadFile(file_name);
+  if (!content.has_value()) {
+    fmt::print("No such file!\n");
+    return 1;
+  }
 
-	// FIXME: Write more robost arg parser for it.
+  // FIXME: Write more robost arg parser for it.
   if (argc == 3) {
     ast_dump_mode = (strcmp(argv[2], "--ast-dump") == 0);
   }
 
-  jcc::Lexer lexer(content, file_name);
+  jcc::Lexer lexer(*content, file_name);
   jcc::Parser parser(lexer);
 
   std::vector<jcc::Decl*> decls = parser.ParseTranslateUnit();
